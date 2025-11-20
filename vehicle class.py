4c442ea1b290f393722833c6e01702b5c2e99378
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-11-20 20:57:51 +1300

    Add files via upload

---
+++ vehicle class.py
@@ -0,0 +1,8 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+joseph = vehicle(365, 20)
+print("josephs max running speed is:", joseph.max_speed, "and still couldn't save CEASARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+print("josephs mileage is:", joseph.mileage, "which might explain it")
+print("joseph:'your next line is: was that a jojos reference?!?!!?'")
